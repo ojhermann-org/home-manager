b49006c4180be76abed849044754847cee15b839
Otto J. Hermann <ojhermann@gmail.com>
2026-04-10 19:35:38 -0400

    feat: declare otto-personal and jump-box Firefox profiles (#73) (#75)

--- programs/firefox.nix
+++ programs/firefox.nix
@@ -1,5 +1,23 @@
 { pkgs, lib, ... }:
 
 lib.mkIf pkgs.stdenv.hostPlatform.isDarwin {
-  programs.firefox.enable = true;
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      otto-personal = {
+        id = 0;
+        isDefault = true;
+      };
+      jump-box = {
+        id = 1;
+        settings = {
+          "network.proxy.type" = 1;
+          "network.proxy.socks" = "localhost";
+          "network.proxy.socks_port" = 1080;
+          "network.proxy.socks_version" = 5;
+          "network.proxy.socks_remote_dns" = true;
+        };
+      };
+    };
+  };
 }
